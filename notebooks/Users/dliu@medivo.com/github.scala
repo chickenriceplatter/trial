363d8f34f1de91eeba06ba45aa626b9f434e31d1
chickenriceplatter <chickenriceplatter@gmail.com>
2016-08-01 14:15:23 -0400

    add example

--- notebooks/Users/dliu@medivo.com/github.scala
+++ notebooks/Users/dliu@medivo.com/github.scala
@@ -1,1 +1,9 @@
 // Databricks notebook source exported at Fri, 29 Jul 2016 22:12:29 UTC
+
+val list = List((1, "one"),(2, "two"),(3,"three"),(4,"four"),(5,"five"))
+
+val rdd = sc.parallelize(list)
+
+val df = rdd.toDF("index", "number")
+
+val new_df = df.withColumn("another", $"index" + 1)
